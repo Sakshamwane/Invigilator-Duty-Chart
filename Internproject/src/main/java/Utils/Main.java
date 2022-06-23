deb23972613f28b54fecebc230c7fdb1c731342e
Satyam Singh <77201460+Satyam-Singh2002@users.noreply.github.com>
2022-06-23 12:30:32 +0530

    23-june

--- Internproject/src/main/java/Utils/Main.java
+++ Internproject/src/main/java/Utils/Main.java
@@ -1,5 +1,15 @@
 package Utils;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		String f1 = "./src/main/java/DataSheet1.xlsx";
+		String f2 = "./src/main/java/DataSheet1.xlsx";
+		
+		String s1 = "Sheet1";
+		String s2 = "Sheet1";
+		
+	    Reader r1 = new Reader(f1, s1, f2, s2);
+	    r1.test();
+	}
 }
